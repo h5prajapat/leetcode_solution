f50f3aaf54c2d7e8a211384714d5d5942c439e6e
h5prajapat <hprajapat.work@gmail.com>
2026-07-16 22:58:12 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.8%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int temp=0;
-        if (x<0) {
-            return false;
-        }else if(x==0){
-            return true;
-        }else if(x%10 == 0){
-            return false;
-        }else{ 
-            while (x>temp){
-                temp = temp*10 + x%10;
-                if (temp == x){
-                    return true;
-                }
-                x = x/10;
-                if (temp == x){
-                    return true;
-                }
-                
+        // h5prajapat
+        int temp = x;
+        int r = 0,d=0;
+
+        if (x<0 )return false;
+        if (x==0)return true;
+        if (x%10 == 0)return false;
+        
+        while (x>0){
+            d=x%10;
+            x=x/10;
+
+            if (r>INT_MAX /10 || (r==INT_MAX/10 && d>7)){
+                return false;
             }
+            r=r*10 + d;
         }
-        return false;
+        return temp == r;
     }
 };
