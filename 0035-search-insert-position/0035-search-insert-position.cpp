f14f70321a3866db3c1f06614ec9014bee41f884
h5prajapat <hprajapat.work@gmail.com>
2026-07-18 12:37:52 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.31%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        // h5prajapat
-        int a=0;
-        int b= 0;
-        int x = 0;
+        // h5prajapat 
+
+
+        // // solution 1
+        // int a=0;
+        // int y = nums.size();
+
+        // for (int i=0 ; i<y ; i++){
+        //     if (nums[i]==target){
+        //         return i;
+        //     }
+        //     if (nums[i]<target){
+        //         a++;
+        //     }
+        // }
+        // return a;
+
+        // // end 1 
+
+
+
+
+
+        // solution 2
+
         int y = nums.size();
 
-        for (int i=x ; i<y ; i++){
+
+        for (int i=0 ; i<y ; i++){
             if (nums[i]==target){
                 return i;
             }
-            if (nums[i]<target){
-                a++;
+            if (nums[i]>target){
+                return i;
             }
         }
-        return a;
+        return y;
+        
+        // end 2
     }
 };
