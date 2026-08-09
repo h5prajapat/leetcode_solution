699c6ca1920ec95754a868e33723e2d230164529
h5prajapat <hprajapat.work@gmail.com>
2026-08-09 17:17:01 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.99%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    int cs = 0;
+    int ms = INT_MIN;  
+    int n = nums.size();
+    
+    for(int i = 0; i < n; i++){
+        cs = cs + nums[i];
+        if(cs > ms){
+            ms = cs;
+        }
+        if(cs < 0){
+            cs = 0;
+        }
+    }
+    
+    return ms;
+}
+        
+};
